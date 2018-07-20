3644e7af7e2c968a3b23a507421578f0761f1ff2
florian <florian.mladitsch@gmail.com>
2018-07-20 20:15:26 +0200

    integration test for product DELETE endpoint

---
+++ EDrinks/Tests/EDrinks.Test.Integration/Endpoints/ProductsController/DeleteProduct.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using EDrinks.Events.Products;
+using Xunit;
+
+namespace EDrinks.Test.Integration.Endpoints.ProductsController
+{
+    public class DeleteProduct : ServiceTest
+    {
+        public DeleteProduct(ServiceFixture fixture) : base(fixture)
+        {
+        }
+
+        [Fact]
+        public async Task TestNonExistantProduct()
+        {
+            var response = await CallEndpoint(Guid.NewGuid());
+            
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task TestDeleteProduct()
+        {
+            var productId = Guid.NewGuid();
+            await WriteToStream(new ProductCreated() {ProductId = productId});
+
+            var response = await CallEndpoint(productId);
+            
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        private async Task<HttpResponseMessage> CallEndpoint(Guid productId)
+        {
+            return await _fixture.Client.DeleteAsync($"/api/Products/{productId}");
+        }
+    }
+}
