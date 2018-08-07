e1f9a8add959b113c66b6cb0d691f6f248c34823
Florian Mladitsch <florian.mladitsch@gmail.com>
2018-08-07 20:10:09 +0200

    placeholder endpoint for creating orders

---
+++ EDrinks/EDrinks.WebApi/Controllers/OrdersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using EDrinks.WebApi.Attributes;
+using EDrinks.WebApi.Dtos;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EDrinks.WebApi.Controllers
+{
+    public class OrdersController : BaseController
+    {
+        private readonly IMediator _mediator;
+
+        public OrdersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [ValidateModel]
+        public async Task<IActionResult> CreateOrder([FromBody] OrderDto order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EDrinks/EDrinks.WebApi/Dtos/OrderDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EDrinks.WebApi.Dtos
+{
+    public class OrderDto
+    {
+        [Required]
+        public Guid TabId { get; set; }
+
+        [Required]
+        public Guid ProductId { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+    }
+}
