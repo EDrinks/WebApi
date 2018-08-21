86adbb5c767e9ccacbfebb47a472319981c78306
Florian Mladitsch <florian.mladitsch@gmail.com>
2018-08-21 19:51:59 +0200

    shitty data generator for tabs

---
+++ EDrinks/EDrinks.Helper.DataFiller/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using Bogus;
+using EDrinks.Events;
+using EDrinks.Events.Tabs;
+using EventStore.ClientAPI;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace EDrinks.Helper.DataFiller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.Development.json", optional: true);
+
+            var configuration = builder.Build();
+
+            var ipAddress = configuration.GetValue<string>("EventStore:IPAddress");
+            var port = configuration.GetValue<int>("EventStore:Port");
+
+            var settings = ConnectionSettings.Create();
+            var connection = EventStoreConnection.Create(settings, new IPEndPoint(IPAddress.Parse(ipAddress), port));
+            connection.ConnectAsync().Wait();
+
+            string stream = args[0];
+            int numberOfTabs = Convert.ToInt32(args[1]);
+
+            FillTabs(connection, stream, numberOfTabs);
+        }
+
+        static void FillTabs(IEventStoreConnection connection, string stream, int numberOfTabs)
+        {
+            var faker = new Faker();
+            
+            for (int i = 0; i < numberOfTabs; i++)
+            {
+                var tabId = Guid.NewGuid();
+
+                connection.AppendToStreamAsync(stream, ExpectedVersion.Any, GetEventData(new TabCreated()
+                {
+                    TabId = tabId
+                })).Wait();
+                connection.AppendToStreamAsync(stream, ExpectedVersion.Any, GetEventData(new TabNameChanged()
+                {
+                    TabId = tabId,
+                    Name = $"{faker.Name.FirstName()} {faker.Name.LastName()}"
+                })).Wait();
+            }
+        }
+
+        static EventData GetEventData(BaseEvent evt)
+        {
+            var metaDataStr = JsonConvert.SerializeObject(evt.MetaData);
+            var contentStr = JsonConvert.SerializeObject(evt);
+
+            return new EventData(Guid.NewGuid(), evt.GetType().Name, true,
+                Encoding.UTF8.GetBytes(contentStr), Encoding.UTF8.GetBytes(metaDataStr));
+        }
+    }
+}
