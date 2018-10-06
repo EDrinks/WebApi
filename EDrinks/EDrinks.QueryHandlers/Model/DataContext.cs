377695b4b900ebb9dfe41ec9842e2183f122b5ef
Florian Mladitsch <florian.mladitsch@gmail.com>
2018-10-06 17:28:19 +0200

    forgot to add class

---
+++ EDrinks/EDrinks.QueryHandlers/Model/DataContext.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+
+namespace EDrinks.QueryHandlers.Model
+{
+    public interface IDataContext
+    {
+        List<Product> Products { get; set; }
+        
+        List<Tab> Tabs { get; set; }
+        
+        List<Order> Orders { get; set; }
+        
+        List<Settlement> Settlements { get; set; }
+        
+        Settlement CurrentSettlement { get; set; }
+    }
+    
+    public class DataContext : IDataContext
+    {
+        public List<Product> Products { get; set; } = new List<Product>();
+        
+        public List<Tab> Tabs { get; set; } = new List<Tab>();
+        
+        public List<Order> Orders { get; set; } = new List<Order>();
+        
+        public List<Settlement> Settlements { get; set; } = new List<Settlement>();
+        
+        public Settlement CurrentSettlement { get; set; } = new Settlement();
+    }
+}
